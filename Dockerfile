5ce989b37fce9f0e605858148d8bab83ef80424a
Maxim Vasilyev <admin@qwertys.ru>
2018-03-15 13:45:09 +0300

    Initial release

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM alpine
+
+RUN apk update
+RUN apk add bash
+RUN apk add curl
+
+COPY . /app
+
+ENV DB_HOST=stores-pg-postgresql
+ENV ES_HOST=elasticsearch
+ENV KC_HOST=kakfa-connect
+
+ENV DB_PORT=5432
+ENV ES_PORT=9200
+ENV KC_PORT=8083
+
+ENV DB_USER=stores
+ENV DB_PASS=stores
+ENV DB=stores
+
+ENTRYPOINT /app/init_connectors.sh
+
---
+++ init_connectors.sh
@@ -0,0 +1,166 @@
+#!/bin/bash
+
+set -u
+
+kc_host=${1-$KC_HOST}
+pg_host=${2-$DB_HOST}
+es_host=${3-$ES_HOST}
+
+kc_port=${4-$KC_PORT}
+pg_port=${5-$DB_PORT}
+es_port=${6-$ES_PORT}
+
+pg_user="${7-$DB_USER}"
+pg_pass="${8-$DB_PASS}"
+pg_db="${9-$DB}"
+
+debezium_json='{
+  "name": "stores-pg-connector",
+  "config": {
+    "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+    "database.user": '\"$pg_user\"',
+    "database.dbname": '\"$pg_db\"',
+    "database.hostname": '\"$pg_host\"',
+    "database.password": '\"$pg_pass\"',
+    "name": "stores-pg-connector",
+    "database.server.name": '\"$pg_host\"',
+    "database.port": '\"$pg_port\"'
+  }
+}'
+
+stores_json='{
+  "name": "stores-connector",
+  "config": {
+    "connector.class": "com.skynyrd.kafka.ElasticSinkConnector",
+    "topics": "stores-pg.public.stores",
+    "tasks.max": "1",
+    "type.name": "_doc",
+    "elastic.url": '\"$es_host\"',
+    "index.name": "stores",
+    "elastic.port": '\"$es_port\"'
+  }
+}'
+
+products_json='{
+  "name": "products-connector",
+  "config": {
+    "connector.class": "com.skynyrd.kafka.ElasticSinkConnector",
+    "topics": "stores-pg.public.products",
+    "tasks.max": "1",
+    "type.name": "_doc",
+    "elastic.url": '\"$es_host\"',
+    "index.name": "products",
+    "elastic.port": '\"$es_port\"'
+  }
+}'
+
+prod_attr_json='{
+  "name": "prod_attr_values-connector",
+  "config": {
+    "connector.class": "com.skynyrd.kafka.ElasticSinkConnector",
+    "topics": "stores-pg.public.prod_attr_values",
+    "tasks.max": "1",
+    "type.name": "_doc",
+    "elastic.url": '\"$es_host\"',
+    "index.name": "products",
+    "elastic.port": '\"$es_port\"'
+  }
+}'
+
+es_stores_json='
+{
+      "mappings": {
+         "_doc": {
+            "properties": {
+               "name": {
+                  "type": "nested",
+                  "properties": {
+                     "lang": {
+                        "type": "text"
+                     },
+                     "text": {
+                        "type": "text"
+                     }
+                  }
+               },
+               "user_id": {
+                  "type": "integer"
+               },
+               "id": {
+                  "type": "integer"
+               }
+            }
+         }
+      }
+}
+'
+
+es_products_json='
+{
+      "mappings": {
+         "_doc": {
+            "properties": {
+               "name": {
+                  "type": "nested",
+                  "properties": {
+                     "lang": {
+                        "type": "text"
+                     },
+                     "text": {
+                        "type": "text"
+                     }
+                  }
+               },
+               "short_description": {
+                  "type": "nested",
+                  "properties": {
+                     "lang": {
+                        "type": "text"
+                     },
+                     "text": {
+                        "type": "text"
+                     }
+                  }
+               },
+               "long_description": {
+                  "type": "nested",
+                  "properties": {
+                     "lang": {
+                        "type": "text"
+                     },
+                     "text": {
+                        "type": "text"
+                     }
+                  }
+               },
+               "id": {
+                  "type": "integer"
+               },
+               "attrs": {
+                  "type": "nested"
+               }
+            }
+         }
+      }
+}
+'
+
+# Checking availability:
+/app/wait_for_it.sh ${pg_host}:${pg_port} || sleep 30
+/app/wait_for_it.sh ${es_host}:${es_port} || sleep 30
+/app/wait_for_it.sh ${kc_host}:${kc_port} || sleep 30
+
+echo "Initializing connectors"
+
+for connector in "$debezium_json" "$stores_json" "$products_json" "$prod_attr_json"
+do
+    curl -si -X POST -H "Accept: application/json" -H "Content-Type: application/json" ${kc_host}:${kc_port}/connectors -d "$connector"
+done
+
+echo
+echo "Initializing Elastic indices"
+
+curl -si -XPUT ${es_host}:${es_port}/stores\?pretty -H 'Content-Type: application/json' -d "$es_stores_json"
+curl -si -XPUT ${es_host}:${es_port}/products\?pretty -H 'Content-Type: application/json' -d "$es_products_json"
+
+exit 0
